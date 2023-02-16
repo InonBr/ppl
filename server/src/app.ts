fe1562184af3a1f4b409b9d8f84ed2ecab2fcae9
Inon Bercu <inonbr@gmail.com>
2023-02-16 11:24:37 +0200

    backend setup

---
+++ server/src/app.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`🟢 App listening at http://localhost:${port}`);
+});
